688125160e5388f622f100ef61f90a4e4f827e08
Dylan Stratton <dm.stratton94@gmail.com>
2018-05-28 13:16:30 -0500

    front end auth

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -4,19 +4,11 @@ import requiresLogin from './requires-login';
 import { fetchProtectedData } from '../actions/protected-data';
 
 export class Dashboard extends React.Component {
-  componentDidMount() {
-    this.props.dispatch(fetchProtectedData());
-  }
-
   render() {
     return (
       <div className="dashboard">
         <div className="dashboard-username">
           Username: {this.props.username}
-        </div>
-        <div className="dashboard-name">Name: {this.props.name}</div>
-        <div className="dashboard-protected-data">
-          Protected data: {this.props.protectedData}
         </div>
       </div>
     );
@@ -26,9 +18,7 @@ export class Dashboard extends React.Component {
 const mapStateToProps = state => {
   const { currentUser } = state.auth;
   return {
-    username: state.auth.currentUser.username,
-    name: `${currentUser.firstName} ${currentUser.lastName}`,
-    protectedData: state.protectedData.data
+    username: state.auth.currentUser.username
   };
 };
 

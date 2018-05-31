ea3383b25ba5667b1a8d35657fef41d5a4b270f7
Dylan Stratton <dm.stratton94@gmail.com>
2018-05-30 22:23:00 -0500

    basic styling for deck management

--- src/components/add-deck-form.js
+++ src/components/add-deck-form.js
@@ -1,53 +1,53 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Field, reduxForm, focus, reset} from 'redux-form';
+import { connect } from 'react-redux';
+import { Field, reduxForm, focus, reset } from 'redux-form';
 import Input from './input';
-import {addDeck} from '../actions/decks';
+import { addDeck } from '../actions/decks';
 import { required, nonEmpty } from '../validators';
 
 
 class AddDeckForm extends React.Component {
-    onSubmit(values) {
-        this.props.dispatch(reset('add-deck-form'))
-        this.props.dispatch(addDeck(this.props.authToken, this.props.userId, values.deckName))
-    }
+  onSubmit(values) {
+    this.props.dispatch(reset('add-deck-form'));
+    this.props.dispatch(addDeck(this.props.authToken, this.props.userId, values.deckName));
+  }
 
-    render() {
-        return (
-            <form className="options__add-item-form" onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
-                <label className="options__add-item-form__label" htmlFor="deckName">Deck Name</label>
-                <Field
-                    component={Input}
-                    type="text"
-                    name="deckName"
-                    id="deckName"
-                    validate={[required, nonEmpty]}
-                />
-                <button
-                    className="options__add-item-form__button"
-                    disabled={this.props.pristine || this.props.submitting}>
-                    Add Item
-                </button>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form className="options__add-item-form login-form" onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
+        <label className="options__add-item-form__label" htmlFor="deckName">Deck Name</label>
+        <Field
+          component={Input}
+          type="text"
+          name="deckName"
+          id="deckName"
+          validate={[required, nonEmpty]}
+        />
+        <button
+          className="options__add-item-form__button"
+          disabled={this.props.pristine || this.props.submitting}>
+          Add Item
+        </button>
+      </form>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    try {
-        return ({
-            authToken: state.auth.authToken,
-            userId: state.auth.currentUser.id
-        })
-    } catch(err){
-        return ({
-            authToken: null,
-            userId: null
-        })
-    }
-}
+  try {
+    return ({
+      authToken: state.auth.authToken,
+      userId: state.auth.currentUser.id
+    });
+  } catch (err) {
+    return ({
+      authToken: null,
+      userId: null
+    });
+  }
+};
 
 export default reduxForm({
-    form: 'add-deck-form',
-    onSubmitFail: (errors, dispatch) => dispatch(focus('add-item-form', 'deckName'))
+  form: 'add-deck-form',
+  onSubmitFail: (errors, dispatch) => dispatch(focus('add-item-form', 'deckName'))
 })(connect(mapStateToProps)(AddDeckForm));
--- src/components/deck-management.js
+++ src/components/deck-management.js
@@ -2,60 +2,63 @@ import React from 'react';
 import { connect } from 'react-redux';
 import requiresLogin from './requires-login';
 import AddDeckForm from './add-deck-form';
-import {getDeckNames, fetchDeckNames, deleteDeck} from '../actions/decks';
-import {Redirect} from 'react-router-dom';
+import { getDeckNames, fetchDeckNames, deleteDeck } from '../actions/decks';
+import { Redirect } from 'react-router-dom';
 
 
 export class DeckManagement extends React.Component {
 
-    dispatchDeleteDeck(deckId) {
-        console.log(this.props.decks.length)
-        if (this.props.decks.length <= 1) {
-            alert('Cannot delete last deck, please add another before deleting.')
-        } else {
-            this.props.dispatch(deleteDeck(this.props.authToken, this.props.userId, deckId))
-        }
+  dispatchDeleteDeck(deckId) {
+    if (this.props.decks.length <= 1) {
+      alert('Cannot delete last deck, please add another before deleting.');
+    } else {
+      this.props.dispatch(deleteDeck(this.props.authToken, this.props.userId, deckId));
     }
+  }
 
   componentDidMount() {
-    this.props.dispatch(getDeckNames(this.props.authToken, this.props.userId))
+    this.props.dispatch(getDeckNames(this.props.authToken, this.props.userId));
   }
 
   render() {
     if (this.props.currentTab === 'dashboard') {
-        return <Redirect to='/dashboard' />
-      }
-    const decks = this.props.decks
+      return <Redirect to='/dashboard' />;
+    }
+    const decks = this.props.decks;
     let arr = [];
 
-    try{
-        decks.forEach((deck) => {
-            arr.push(deck)
-        })
-    } catch(err) {}
-    const buildJSX = arr.map((deck) => {
-        return (
-            <li value={deck.id} id={deck.id} key={deck.id}>
-                <p>{deck.name}</p>
-                <button value={deck.id} 
-                onClick={(event) => this.dispatchDeleteDeck(event.target.value)}>
-                Delete</button>
-            </li>
-        )
-    })
-    return (
-        <div>
-            <AddDeckForm/>
-            <ul>
-                {buildJSX}
-            </ul>
-        </div>
-    );
+    try {
+      decks.forEach((deck) => {
+        arr.push(deck);
+      });
+    } catch (err) {
+      console.log(err);
     }
+    const buildJSX = arr.map((deck) => {
+      return (
+        <li className="deck-list__item" value={deck.id} id={deck.id} key={deck.id}>
+          <p className="deck-list__name">{deck.name}</p>
+          <button className="deck-list__delete" value={deck.id}
+            onClick={(event) => this.dispatchDeleteDeck(event.target.value)}>
+            Delete</button>
+        </li>
+      );
+    });
+    return (
+      <div className="decks">
+        <div className="decks__wrap">
+          <AddDeckForm />
+          <p>Current Decks:</p>
+          <ul className="deck-list">
+            {buildJSX}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    console.log(state)
   return {
     authToken: state.auth.authToken,
     decks: state.deckReducer.deckNames,

c9573bcbc33c2037ea060c0132369af7c87dd040
Dylan Stratton <dm.stratton94@gmail.com>
2018-05-31 14:40:39 -0500

    add footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Coded with love by Christopher Whiteman and Dylan Stratton</p>
+      <p>Coded with love by <a className="footer__link" href="https://www.chris-whiteman.com">Christopher Whiteman</a> and <a className="footer__link" href="http://www.dylanstratton.com/">Dylan Stratton</a></p>
     </div>
   );
 };

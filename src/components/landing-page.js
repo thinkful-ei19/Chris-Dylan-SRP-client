f65a4f2c5ad51e9abb30c4d951aa782428e3141a
Christopher <cjszk@hotmail.com>
2018-06-30 21:26:04 -0700

    Added GET request on landing-page for heroku

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
-
+import { API_BASE_URL } from '../config';
 import LoginForm from './login-form';
 import RegistrationForm from './registration-form';
 
 export function LandingPage(props) {
+
+  //This below GET request is to wake up the heroku server when a user loads up the landing page.
+  //This is so that the heroku server doesn't take forever to allow the user to log in.
+  fetch(`${API_BASE_URL}`, {
+    method: 'GET', 
+    headers: {
+        'Accept': 'application/json',
+      }
+})
+.then(res => console.log('Sent artbitrary get request to heroku server to wake it up...', res)).catch(err => {});
+
+
   // If we are logged in redirect straight to the user's dashboard
   if (props.loggedIn) {
     return <Redirect to="/dashboard" />;
